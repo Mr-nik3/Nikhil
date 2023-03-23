ccc05d8a8ec9d1981f1d7c2c608527470c6562c6
Mr-nik3 <126542752+Mr-nik3@users.noreply.github.com>
2023-03-23 17:00:55 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,57 @@
+
+
+</div><!-- #Wrapper -->
+
+<div id="Side_slide" class="right dark" data-width="250"><div class="close-wrapper"><a href="#" class="close"><i class="icon-cancel-fine"></i></a></div><div class="extras"><div class="extras-wrapper"><a class="icon cart" id="slide-cart" href="index.html"><i class="icon-bag-fine"></i><span>0</span></a><a class="icon search" href="#"><i class="icon-search-fine"></i></a></div></div><div class="search-wrapper"><form id="side-form" method="get" action="https://#/"><input type="text" class="field" name="s" placeholder="Enter your search" /><input type="submit" class="submit" value="" style="display:none;" /><a class="submit" href="#"><i class="icon-search-fine"></i></a></form></div><div class="lang-wrapper"></div><div class="menu_wrapper"></div><ul class="social"><li class="whatsapp"><a target="_blank" href="#" title="WhatsApp"><i class="icon-whatsapp"></i></a></li><li class="facebook"><a target="_blank" href="#" title="Facebook"><i class="icon-facebook"></i></a></li><li class="twitter"><a target="_blank" href="#" title="Twitter"><i class="icon-twitter"></i></a></li><li class="linkedin"><a target="_blank" href="#" title="LinkedIn"><i class="icon-linkedin"></i></a></li></ul></div><div id="body_overlay"></div>
+
+
+<!-- mfn_hook_bottom --><!-- mfn_hook_bottom -->	
+<!-- wp_footer() -->
+
+
+	<script type="text/javascript">
+		var c = document.body.className;
+		c = c.replace(/woocommerce-no-js/, 'woocommerce-js');
+		document.body.className = c;
+	</script>
+				<script type="text/javascript">
+				function revslider_showDoubleJqueryError(sliderID) {
+					var errorMessage = "Revolution Slider Error: You have some jquery.js library include that comes after the revolution files js include.";
+					errorMessage += "<br> This includes make eliminates the revolution slider libraries, and make it not work.";
+					errorMessage += "<br><br> To fix it you can:<br>&nbsp;&nbsp;&nbsp; 1. In the Slider Settings -> Troubleshooting set option:  <strong><b>Put JS Includes To Body</b></strong> option to true.";
+					errorMessage += "<br>&nbsp;&nbsp;&nbsp; 2. Find the double jquery.js include and remove it.";
+					errorMessage = "<span style='font-size:16px;color:#BC0C06;'>" + errorMessage + "</span>";
+						jQuery(sliderID).show().html(errorMessage);
+				}
+			</script>
+			<link rel='stylesheet' id='images-continuous-thumbnail-slider-plus-lighbox-style-css'  href='<?=base_url()?>application/libraries/wp-content/plugins/continuous-image-carousel-with-lightbox/css/images-continuous-thumbnail-slider-plus-lighbox-style1c96.css?ver=1.0.10' type='text/css' media='all' />
+<link rel='stylesheet' id='continuous-l-box-css-css'  href='<?=base_url()?>application/libraries/wp-content/plugins/continuous-image-carousel-with-lightbox/css/continuous-l-box-css1c96.css?ver=1.0.10' type='text/css' media='all' />
+
+<script type='text/javascript' src='<?=base_url()?>application/libraries/wp-content/plugins/contact-form-7/includes/js/scripts3c21.js?ver=5.1.1' id='contact-form-7-js'></script>
+<script type='text/javascript' src='<?=base_url()?>application/libraries/wp-content/plugins/woocommerce/assets/js/jquery-blockui/jquery.blockUI.min44fd.js?ver=2.70' id='jquery-blockui-js'></script>
+
+<script type='text/javascript' src='<?=base_url()?>application/libraries/wp-content/plugins/woocommerce/assets/js/frontend/add-to-cart.minb284.js?ver=3.5.10' id='wc-add-to-cart-js'></script>
+<script type='text/javascript' src='<?=base_url()?>application/libraries/wp-content/plugins/woocommerce/assets/js/js-cookie/js.cookie.min6b25.js?ver=2.1.4' id='js-cookie-js'></script>
+
+<script type='text/javascript' src='<?=base_url()?>application/libraries/wp-content/plugins/woocommerce/assets/js/frontend/cart-fragments.minb284.js?ver=3.5.10' id='wc-cart-fragments-js'></script>
+<script type='text/javascript' src='<?=base_url()?>application/libraries/wp-includes/js/jquery/ui/core.min35d0.js?ver=1.12.1' id='jquery-ui-core-js'></script>
+<script type='text/javascript' src='<?=base_url()?>application/libraries/wp-includes/js/jquery/ui/mouse.min35d0.js?ver=1.12.1' id='jquery-ui-mouse-js'></script>
+<script type='text/javascript' src='<?=base_url()?>application/libraries/wp-includes/js/jquery/ui/sortable.min35d0.js?ver=1.12.1' id='jquery-ui-sortable-js'></script>
+<script type='text/javascript' src='<?=base_url()?>application/libraries/wp-includes/js/jquery/ui/tabs.min35d0.js?ver=1.12.1' id='jquery-ui-tabs-js'></script>
+<script type='text/javascript' src='<?=base_url()?>application/libraries/wp-includes/js/jquery/ui/accordion.min35d0.js?ver=1.12.1' id='jquery-ui-accordion-js'></script>
+<script type='text/javascript' src='<?=base_url()?>application/libraries/wp-content/themes/betheme/js/plugins8f85.js?ver=20.8.2' id='jquery-plugins-js'></script>
+<script type='text/javascript' src='<?=base_url()?>application/libraries/wp-content/themes/betheme/js/menu8f85.js?ver=20.8.2' id='jquery-mfn-menu-js'></script>
+<script type='text/javascript' src='<?=base_url()?>application/libraries/wp-content/themes/betheme/assets/animations/animations.min8f85.js?ver=20.8.2' id='jquery-animations-js'></script>
+<script type='text/javascript' src='<?=base_url()?>application/libraries/wp-content/themes/betheme/assets/jplayer/jplayer.min8f85.js?ver=20.8.2' id='jquery-jplayer-js'></script>
+<script type='text/javascript' src='<?=base_url()?>application/libraries/wp-content/themes/betheme/js/parallax/translate3d8f85.js?ver=20.8.2' id='jquery-mfn-parallax-js'></script>
+<script type='text/javascript' src='<?=base_url()?>application/libraries/wp-content/themes/betheme/js/parallax/smoothscroll8f85.js?ver=20.8.2' id='jquery-smoothscroll-js'></script>
+<script type='text/javascript' src='<?=base_url()?>application/libraries/wp-content/themes/betheme/js/scripts8f85.js?ver=20.8.2' id='jquery-scripts-js'></script>
+
+<script type='text/javascript' src='<?=base_url()?>application/libraries/wp-content/plugins/woocommerce-catalog-enquiry/assets/frontend/js/frontend5fc8.js?ver=4.0.4' id='wce_frontend_js-js'></script>
+<script type='text/javascript' src='<?=base_url()?>application/libraries/wp-includes/js/wp-embed.min6281.js?ver=5.8.6' id='wp-embed-js'></script>
+<script type='text/javascript' src='<?=base_url()?>application/libraries/wp-content/plugins/continuous-image-carousel-with-lightbox/js/images-continuous-thumbnail-slider-plus-lightbox-jc7daa.js?ver=1.0.13' id='images-continuous-thumbnail-slider-plus-lightbox-jc-js'></script>
+<script type='text/javascript' src='<?=base_url()?>application/libraries/wp-content/plugins/continuous-image-carousel-with-lightbox/js/continuous-l-box-js0718.js?ver=1.0.12' id='continuous-l-box-js-js'></script>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,316 @@
+
+
+			
+		</div>
+
+		
+		<!-- mfn_hook_content_before --><!-- mfn_hook_content_before -->	
+<!-- #Content -->
+<div id="Content">
+	<div class="content_wrapper clearfix">
+
+		<!-- .sections_group -->
+		<div class="sections_group">
+		
+			<div class="entry-content" itemprop="mainContentOfPage">
+			
+				<div class="section mcb-section   " id="prdtlist" style="padding-top:0px; padding-bottom:0px; background-color:" ><div class="section_wrapper mcb-section-inner"><div class="wrap mcb-wrap one  valign-top clearfix" style=""  ><div class="mcb-wrap-inner"><div class="column mcb-column one column_column "><div class="column_attr clearfix"  style=""><div class="container">
+  <div class="row">
+      <div class="col-md-3 col-sm-6">
+        <div class="serviceBox green">
+            <div class="service-content">
+              <div class="service-icon">
+                  <i class="fa fa-users"></i>
+              </div>
+                <h3 class="title">Cheif Guest</h3>
+                <p class="description">
+                 
+Anandiben Mafatbhai Patel (born 21 November 1941) is an Indian politician serving as the 20th and current Governor of Uttar Pradesh. She also served as 17th Governor of Madhya Pradesh and Governor of Chhattisgarh. She has served as the first female Chief Minister of Gujarat.
+                </p>
+            </div>
+            <a class="read-more" href="products/index.html" target="_blank">Anandiben Patel</a>
+        </div>
+      </div>
+	  
+	   
+<div class="col-md-3 col-sm-6">
+          <div class="serviceBox">
+              <div class="service-content">
+                <div class="service-icon">
+                    <i class="fa fa-globe"></i>
+                </div>
+                  <h3 class="title">Cheif Guest</h3>
+                  <p class="description">
+Chidananda Saraswati  was President of the Divine Life Society, Rishikesh, India. He is well known in India as a yogi, dnyani and spiritual leader. He succeeded as President of the Divine Life Society in 1963, after the death of his predecessor, Sivananda Saraswati, who founded the Society. </p>
+              </div>
+              <a class="read-more" href="products/index.html" target="_blank">Swami Chidananda Saraswati</a>
+          </div>
+      </div>
+<div class="col-md-3 col-sm-6">
+          <div class="serviceBox">
+              <div class="service-content">
+                <div class="service-icon">
+                    <i class="fa fa-globe"></i>
+                </div>
+                  <h3 class="title">Cheif Guest</h3>
+                  <p class="description">
+Yogendra Upadhyaya is an Indian politician and member of the 18th Uttar Pradesh Assembly and was also 16th Legislative Assembly of Uttar Pradesh. Upadhyaya is a member of the Bharatiya Janata Party and represented Agra South constituency of Uttar Pradesh. </p>
+              </div>
+              <a class="read-more" href="products/index.html" target="_blank">Yogendra Upadhyaya</a>
+          </div>
+      </div>
+  </div>
+</div>
+
+
+
+
+  </div>
+</div></div></div></div></div></div></div><div class="section mcb-section   " id="second" style="padding-top:50px; padding-bottom:25px; background-color:#f66709" ><div class="section_wrapper mcb-section-inner"><div class="wrap mcb-wrap one  valign-top clearfix" style=""  ><div class="mcb-wrap-inner"><div class="column mcb-column one column_column "><div class="column_attr clearfix"  style=""><h1>37th Convocation Day</h1>
+<hr class="style-two"></hr>
+<div class="brd1"></div>
+<div class="brd2"></div>
+<p>Today, on the occasion of the 37th Convocation of Chhatrapati Shahu Ji Maharaj University, "Amrit Sarovar" and Seva Udyan were inaugurated by the lotus hands of Smt. Anandiben Patel, Hon'ble Chancellor and the Governor. During this program Honorable Vice Chancellor Prof. Vinay Kumar Pathak and other special guests Mr. Yogendra Upadhyay, Hon'ble Minister of Higher Education, Smt. Rajni Tiwari, Hon'ble Minister of State, Higher Education were present.
+Today, on the auspicious occasion of the inauguration of Seva Udyan, Her Excellency Governor Smt. Anandiben Patel arrived, she inaugurated the Seva Udyan and as per the conversation with the volunteers of NSS UNIT 4, the volunteer told that this model has been made from waste material for birds. , So that the bird can take water, eat, rest, as well as His Excellency said that you people have to take special care of these models and keep grains of wheat, rice, pulses in them.
+</p></div></div></div></div></div></div><div class="section mcb-section   " id="third" style="padding-top:50px; padding-bottom:25px; background-color:#ffffff" >
+</div><div class="section mcb-section   " id="prdts" style="padding-top:50px; padding-bottom:25px; background-color:#ffffff" ><div class="section_wrapper mcb-section-inner"><div class="wrap mcb-wrap one  valign-top clearfix" style=""  ><div class="mcb-wrap-inner"><div class="column mcb-column one column_column sldr"><div class="column_attr clearfix align_center"  style=""><h2>Our Gallery</h2>
+<!-- print_continuous_slider_plus_lightbox_func --><style type='text/css' >
+        .bx-wrapper .bx-viewport {
+            background: none repeat scroll 0 0 #FFFFFF !important;
+            border: 0px none !important;
+            box-shadow: 0 0 0 0 !important;
+            /*padding:30px !important;*/
+        }
+    </style>              
+    <div style="clear: both;"></div>
+        <div style="width: auto;postion:relative" id="divSliderMain">
+        
+                         <script>
+         function clickedItem(){ 
+                            
+                          
+                          var uniqObj=jQuery("a[rel^='slider_640b2ccebbcbf']"); 
+                          jQuery(".lightbox_rel640b2ccebbcbe").fancybox_crl({
+                            'overlayColor':'#000000',
+                             'padding': 10,
+                             'autoScale': true,
+                             'autoDimensions':true,
+                             'transitionIn': 'none',
+                             'uniqObj':uniqObj,
+                             'transitionOut': 'none',
+                             'titlePosition': 'over',
+                             'cyclic':true,
+                             'hideOnContentClick':false,
+                             'width' : 600,
+                             'height' : 350,
+                             'titleFormat': function(title, currentArray, currentIndex, currentOpts) {
+                                 var currtElem = jQuery('.responsiveContinuousCarousel a[href="'+currentOpts.href+'"]');
+
+                                 var isoverlay = jQuery(currtElem).attr('data-overlay')
+                                 
+                                if(isoverlay=="1" && jQuery.trim(title)!=""){
+                                 return '<span id="fancybox_crl-title-over">' + title  + '</span>';
+                                }
+                                else{
+                                    return '';
+                                }
+
+                                },
+
+                           });
+
+                           return false;
+                      }
+         </script>
+                 <div class="responsiveContinuousCarousel" style="margin-top: 5px !important;display: none;">
+                     
+            
+
+                    <div class="limargin"> 
+                       <a rel="slider_640b2ccebbcbf" data-overlay="1" data-title="<a class='Imglink' href='#'>ANTOCID CAPSULES HOLY 02</a>" class="lightbox_rel640b2ccebbcbe"   href="<?=base_url()?>application/libraries/web/images/convocation3.jpeg" >
+                            <img  onclick="return clickedItem();"  src="<?=base_url()?>application/libraries/web/images/convocation3.jpeg" alt="ANTOCID CAPSULES HOLY 02" title="ANTOCID CAPSULES HOLY 02"  />
+                        </a> 
+                    </div>
+
+                             
+            
+
+                    <div class="limargin"> 
+                       <a rel="slider_640b2ccebbcbf" data-overlay="1" data-title="<a class='Imglink' href='#'>ANTOCID SYRUP 100 ML HOLY 03</a>" class="lightbox_rel640b2ccebbcbe"   href="<?=base_url()?>application/libraries/web/images/convocation4.jpeg" >
+                            <img  onclick="return clickedItem();"  src="<?=base_url()?>application/libraries/web/images/convocation4.jpeg" alt="ANTOCID SYRUP 100 ML HOLY 03" title="ANTOCID SYRUP 100 ML HOLY 03"  />
+                        </a> 
+                    </div>
+
+                             
+            
+
+                    <div class="limargin"> 
+                       <a rel="slider_640b2ccebbcbf" data-overlay="1" data-title="<a class='Imglink' href='#'>ARTHOVEDA OIL HOLY 03</a>" class="lightbox_rel640b2ccebbcbe"   href="<?=base_url()?>application/libraries/web/images/convocation5.jpeg" >
+                            <img  onclick="return clickedItem();"  src="<?=base_url()?>application/libraries/web/images/convocation5.jpeg" alt="ARTHOVEDA OIL HOLY 03" title="ARTHOVEDA OIL HOLY 03"  />
+                        </a> 
+                    </div>
+
+                             
+            
+
+                    <div class="limargin"> 
+                       <a rel="slider_640b2ccebbcbf" data-overlay="1" data-title="<a class='Imglink' href='#'>ARTHOVEDA CAPSULES HOLY 03</a>" class="lightbox_rel640b2ccebbcbe"   href="<?=base_url()?>application/libraries/web/images/img5.png" >
+                            <img  onclick="return clickedItem();"  src="<?=base_url()?>application/libraries/web/images/convocation6.jpeg" alt="ARTHOVEDA CAPSULES HOLY 03" title="ARTHOVEDA CAPSULES HOLY 03"  />
+                        </a> 
+                    </div>
+
+                             
+            
+
+                    <div class="limargin"> 
+                       <a rel="slider_640b2ccebbcbf" data-overlay="1" data-title="<a class='Imglink' href='#'>ANTOCID CAPSULES HOLY 02</a>" class="lightbox_rel640b2ccebbcbe"   href="<?=base_url()?>application/libraries/web/images/img6.png" >
+                            <img  onclick="return clickedItem();"  src="<?=base_url()?>application/libraries/web/images/convocation7.jpeg" alt="ANTOCID CAPSULES HOLY 02" title="ANTOCID CAPSULES HOLY 02"  />
+                        </a> 
+                    </div>       
+        </div>
+    </div>                   
+    <script>
+
+                     
+        var interval_640b2ccebbde8 = setInterval(function() {
+
+        if(document.readyState === 'complete') {
+
+           clearInterval(interval_640b2ccebbde8);
+                
+                     jQuery(".responsiveContinuousCarousel").show();
+                    var uniqObj=jQuery("a[rel^='slider_640b2ccebbcbf']");
+                         var slider= jQuery('.responsiveContinuousCarousel').bxSlider({
+                             slideWidth: 250,
+                            minSlides: 1,
+                            maxSlides: 5,
+                            slideMargin:30,  
+                            speed:25000,
+                            useCSS:false,
+                            ticker: true,
+                                                            tickerHover:true, 
+                                        
+                                                           captions:false,
+                                                        onSliderLoad: function(){
+                                   
+                            
+                            }    
+
+
+                     });
+
+
+              }    
+                }, 100);
+
+
+
+
+             window.addEventListener('load', function() {
+
+
+                setTimeout(function(){ 
+
+                        if(jQuery(".responsiveContinuousCarousel").find('.bx-loading').length>0){
+
+                                jQuery(".responsiveContinuousCarousel").find('img').each(function(index, elm) {
+
+                                        if(!elm.complete || elm.naturalWidth === 0){
+
+                                            var toload='';
+                                            var toloadval='';
+                                            jQuery.each(this.attributes, function(i, attrib){
+
+                                                    var value = attrib.value;
+                                                    var aname=attrib.name;
+
+                                                    var pattern = /^((http|https):\/\/)/;
+
+                                                    if(pattern.test(value) && aname!='src') {
+
+                                                            toload=aname;
+                                                            toloadval=value;
+                                                     }
+                                                    // do your magic :-)
+                                             });
+
+                                                    vsrc=jQuery(elm).attr("src");
+                                                    jQuery(elm).removeAttr("src");
+                                                    dsrc=jQuery(elm).attr("data-src");
+                                                    lsrc=jQuery(elm).attr("data-lazy-src");
+
+
+                                                       if(dsrc!== undefined && dsrc!='' && dsrc!=vsrc){
+                                                                                     jQuery(elm).attr("src",dsrc);
+                                                            }
+                                                            else if(lsrc!== undefined && lsrc!=vsrc){
+
+                                                                             jQuery(elm).attr("src",lsrc);
+                                                            }
+                                                            else if(toload!='' && toload!='srcset' && toloadval!='' && toloadval!=vsrc){
+
+                                                                    jQuery(elm).removeAttr(toload);
+                                                                    jQuery(elm).attr("src",toloadval);
+
+
+                                                                } 
+                                                            else{
+
+                                                                            jQuery(elm).attr("src",vsrc);
+
+                                                       }   
+
+                                                    elm=jQuery(elm)[0];      
+                                                     if(!elm.complete && elm.naturalHeight == 0){
+
+                                                    jQuery(elm).removeAttr('loading');
+                                                    jQuery(elm).removeAttr('data-lazy-type');
+
+
+                                                    jQuery(elm).removeClass('lazy');
+
+                                                    jQuery(elm).removeClass('lazyLoad');
+                                                    jQuery(elm).removeClass('lazy-loaded');
+                                                    jQuery(elm).removeClass('jetpack-lazy-image');
+                                                    jQuery(elm).removeClass('jetpack-lazy-image--handled');
+                                                    jQuery(elm).removeClass('lazy-hidden');
+
+                                                }
+                                         }
+
+                                    }).promise().done( function(){ 
+
+                                            jQuery(".responsiveContinuousCarousel").find('.bx-loading').remove();
+                                    } );
+
+                            }
+
+
+                   }, 6000);
+
+        });
+                                
+            
+    </script><!-- end print_continuous_slider_plus_lightbox_func --></div></div></div></div></div></div><div class="section the_content no_content"><div class="section_wrapper"><div class="the_content_wrapper"></div></div></div>				
+				<div class="section section-page-footer">
+					<div class="section_wrapper clearfix">
+					
+						<div class="column one page-pager">
+													</div>
+						
+					</div>
+				</div>
+				
+			</div>
+			
+				
+		</div>
+		
+		<!-- .four-columns - sidebar -->
+		
+	</div>
+</div>
+
+
+<!-- mfn_hook_content_after --><!-- mfn_hook_content_after -->
+<!-- #Footer -->
